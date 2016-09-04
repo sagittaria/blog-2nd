15ea5811b1ba27ca6ce4186183e2d8fd81e2dc5d
sagittaria <shuyang_x@hotmail.com>
2016-09-04 18:51:29 +0800

    handle store-operation

--- routes/account.js
+++ routes/account.js
@@ -1,4 +1,5 @@
 var router = require('express').Router();
+var User = require('../models/user');
 
 router.route('/register')
     .get(function (req, res) {
@@ -12,8 +13,15 @@ router.route('/register')
             return res.status(400).end('用户名或密码不合法');
         }
 
+        var newUser = new User({username: username, password: password});
+
+        newUser.save(function(err) {
+            if (err) return next(err);    // 交给接下来的错误处理中间件
+            res.status(201).end('注册成功');       // 存储成功
+        });
+
         // 将来会在这里检查用户名是否存在，我们先把它设为true
-        var usernameExist = true;
+        /*var usernameExist = true;
 
         if(usernameExist){
             return res.status(400).end('用户名已存在');
@@ -21,7 +29,7 @@ router.route('/register')
 
         // 将来会在这里执行用户、密码的存储
 
-        res.status(201).end('注册成功');
+        res.status(201).end('注册成功');*/
     });
 
 module.exports = router;
