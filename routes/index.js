98fd311e56deb318f98d5a1f392b6d0115524a2f
sagittaria <shuyang_x@hotmail.com>
2016-09-05 00:35:21 +0800

    修改之，为看例子的实际显示效果

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var user = {
+    id: 1,
+    username: 'alice',
+    title: 'alice 的博客',
+    description: 'Alice是个好人`',
+    avatar: '/images/default-avatar.jpg'
+};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home/index',{
+    users: [user, user, user],
+    title: 'TMY BLOG: 一个简单的博客系统'
+  });
 });
 
 module.exports = router;
