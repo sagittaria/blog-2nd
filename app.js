c61e6f20b61c423440e790b6fe0e8c3eac30dcb7
sagittaria <shuyang_x@hotmail.com>
2016-09-04 18:49:19 +0800

    set up db connection

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var hbs = require('hbs');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
