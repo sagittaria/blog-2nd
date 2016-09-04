371a4439d9da7e8d4cb6ed5f784b4b06bbc9a303
sagittaria <shuyang_x@hotmail.com>
2016-09-04 16:49:48 +0800

    add server-side validation

--- routes/account.js
+++ routes/account.js
@@ -5,13 +5,23 @@ router.route('/register')
         res.render('account/register', {title: '注册'});
     })
     .post(function (req, res, next) {
-        var username = req.body.username,
-            password = req.body.password;
+        var username = req.body.username || '',
+            password = req.body.password || '';
 
-        console.log('Register post received!');
-        console.log('username:', username, 'password:', password);
-        res.setHeader('content-type','text/html; charset=UTF-8');
-        res.end('成功收到POST请求');
+        if(username.length === 0 || password.length === 0){
+            return res.status(400).end('用户名或密码不合法');
+        }
+
+        // 将来会在这里检查用户名是否存在，我们先把它设为true
+        var usernameExist = true;
+
+        if(usernameExist){
+            return res.status(400).end('用户名已存在');
+        }
+
+        // 将来会在这里执行用户、密码的存储
+
+        res.status(201).end('注册成功');
     });
 
 module.exports = router;
