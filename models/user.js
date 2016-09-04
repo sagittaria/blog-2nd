34b13d8bda4a211d0ad27ffdb855b0c5e9614866
sagittaria <shuyang_x@hotmail.com>
2016-09-04 18:53:32 +0800

    create user model(something like a blueprint of users)

---
+++ models/user.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  username:{type:String,index:{unique:true}},
+  password:String,
+  avatar:{
+    type:String,
+    default:'./public/images/default-avatar.jpg'
+  },
+  title:{
+    type:String,
+    default:'Nameless'
+  },
+  description:{
+    type:String,
+    default:'Nothing here.'
+  }
+});
+
+module.exports = mongoose.model('User', UserSchema);
